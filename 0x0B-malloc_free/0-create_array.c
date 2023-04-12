06a159d7c9727a17474d9e1b84d145f4c7d9124d
Love-sosa <nanabaffour163@gmail.com>
2023-04-12 02:39:36 +0000

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,18 +9,18 @@
 *
 */
 
-char *create_array(unsigned int size, char c) 
+char *create_array(unsigned int size, char c)
 {
 char *str;
 unsigned int i;
 
 str = malloc(size * sizeof(char));
-if (str == NULL) 
+if (str == NULL)
 {
 return (NULL);
 }
 
-for (i = 0; i < size; i++) 
+for (i = 0; i < size; i++)
 {
 str[i] = c;
 }
