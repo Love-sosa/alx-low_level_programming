2bf16904334af845d10fdb273b406592b05a1b3a
Love-sosa <nanabaffour163@gmail.com>
2023-04-18 15:28:23 +0000

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define FILE_NAME __FILE__
+
+/**
+* main - prints the name of the file
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("%s\n", FILE_NAME);
+return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define FILE_NAME __FILE__
+
+/**
+* main - prints the name of the file
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("The name of the compiled file is %s\n", FILE_NAME);
+return (0);
+}
